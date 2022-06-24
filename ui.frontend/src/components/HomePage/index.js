09e5a302accf15a3fd639d2d496d5e6e2fcfc03f
PColares <siqueira.pedro1998@gmail.com>
2022-06-24 14:58:13 -0300

    Add weather api functionality

--- ui.frontend/src/components/HomePage/index.js
+++ ui.frontend/src/components/HomePage/index.js
@@ -1,13 +1,48 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react';
 import { MapTo } from '@adobe/aem-react-editable-components';
-
 require('./index.css')
 
 export const HomePage = ({ title, subtitle, logoImage = {}, backgroundImage = {} }) => {
+    const [lat, setLat] = useState([]);
+    const [long, setLong] = useState([]);
+    const [location, setLocation] = useState(null);
+    const [data, setData] = useState(null);
+
+    const fetchData = async () => {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            setLat(position.coords.latitude);
+            setLong(position.coords.longitude);
+        });
+
+        await fetch('http://api.weatherapi.com/v1/current.json?key=91d0c33bbe064dc3bd0143610222306&q=Belém&aqi=no')
+            .then(response => { if (response.status === 200) { return response.json() } })
+            .then(data => {
+                console.log('data', data)
+                setLocation(data)
+                setData(data.location)
+            });
+    }
+
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
     return (
         <div className="home-container">
             <header className="header-item-1">
                 <img src={logoImage.src} alt='logo' className="compasso-logo-2" />
+                <div className="location">
+                    {location && data && (
+                        <div>
+                            <p>{data.name}</p>
+                            <p>{data.region}</p>
+                            <p>{data.localtime}</p>
+                            <p>{location.current.temp_c}</p>
+                        </div>)
+                    }
+                </div>
             </header>
             <div className="body-container-2">
                 <img src={backgroundImage.src} alt='logo' className="background-image-2" />
@@ -18,7 +53,7 @@ export const HomePage = ({ title, subtitle, logoImage = {}, backgroundImage = {}
                     <p>transformar o mundo</p>
                     <h1>building digital experiences</h1>
                     <p>construindo experiências digitais</p>
-                    <h1>that enable our client’s growth</h1>
+                    <h1>that enable our client's growth</h1>
                     <p>que permitam o crescimento dos nossos clientes</p>
                 </div>
             </div>
